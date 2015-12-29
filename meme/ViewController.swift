8b96fe0a1151cdac53a0eab377c3d98f1c3a07f3
Duong Nhat Thoi <dnhthoi@gmail.com>
2015-12-29 17:31:30 +0700

    create project

---
+++ meme/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  meme
+//
+//  Created by SwagSoft Vn on 12/29/15.
+//  Copyright © 2015 SwagSoft Vn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
